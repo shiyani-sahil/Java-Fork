35a8a5a39aef7315b19a8c25d8546e24db3d31c6
Shiyani Sahil <shiyanisahil3@gmail.com>
2025-08-24 18:36:29 +0530

    Add files via upload

---
+++ p2.java
@@ -0,0 +1,9 @@
+public class p2 {
+    public static void main(String args[])
+    {
+        System.out.println("numer divisible by 5 not by 10");
+        for(int i=1;i<= 100;i++)
+            if(i%5==0 && i%10 != 0)
+                System.out.println(i);
+    }
+}
