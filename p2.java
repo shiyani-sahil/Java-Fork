2f5d92cd8576eebb2e126733b97de83938c54fa1
Shiyani Sahil <shiyanisahil3@gmail.com>
2025-08-24 18:42:22 +0530

    new program add

--- p2.java
+++ p2.java
@@ -1,7 +1,7 @@
 public class p2 {
     public static void main(String args[])
     {
-        System.out.println("numer divisible by 5 not by 10");
+        System.out.println("Number divisible by 5 not by 10");
         for(int i=1;i<= 100;i++)
             if(i%5==0 && i%10 != 0)
                 System.out.println(i);
